4d1ac7570fb0151be6e15261bd522e5010909c02
matyamod <matyamod@gmail.com>
2025-01-30 22:30:15 +0900

    add an option to disable recursive search for the game directory

--- UnrealReZen/Program.cs
+++ UnrealReZen/Program.cs
@@ -35,6 +35,9 @@ namespace UnrealReZen
         [Option("mount-point", Required = false, Default = "../../../", HelpText = "Mount point of packed archive")]
         public string MountPoint { get; set; }
 
+        [Option("game-dir-top-only", Required = false, HelpText = "When enabled, restricts the game directory search to the top-level only.")]
+        public bool GameDirTopOnly { get; set; }
+
         [Usage(ApplicationAlias = "UnrealReZen.exe")]
         public static IEnumerable<Example> Examples
         {
@@ -98,6 +101,7 @@ namespace UnrealReZen
             Console.WriteLine($"Output Path: {opts.OutputPath}");
             Console.WriteLine($"Compression Format: {opts.CompressionFormat}");
             Console.WriteLine($"Mount Point: {opts.MountPoint}");
+            Console.WriteLine($"Game Dir Top Only: {opts.GameDirTopOnly}");
 
             var aesKey = new FAesKey(opts.AESKey ?? Constants.DefaultAES);
             DefaultFileProvider provider;
@@ -105,7 +109,8 @@ namespace UnrealReZen
             Log.Information("Loading Game Archives...");
             try
             {
-                provider = new DefaultFileProvider(opts.GameDirectory, SearchOption.AllDirectories, true, new VersionContainer((EGame)engineVersion));
+                var searchOption = opts.GameDirTopOnly ? SearchOption.TopDirectoryOnly : SearchOption.AllDirectories;
+                provider = new DefaultFileProvider(opts.GameDirectory, searchOption, true, new VersionContainer((EGame)engineVersion));
                 provider.Initialize();
                 provider.SubmitKey(new FGuid(), aesKey);
                 provider.LoadLocalization(ELanguage.English);
